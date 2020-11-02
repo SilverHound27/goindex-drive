8636755ad8bfd9dff30ce2c0abce93df76c5f389
SilverHound27 <62786183+SilverHound27@users.noreply.github.com>
2020-11-02 11:41:24 +0530

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -301,7 +301,7 @@ function file_video(path){
 	  <textarea class="mdui-textfield-input"><video><source src="${url}" type="video/mp4"></video></textarea>
 	</div>
 </div>
-<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+
 	`;
 	$('#content').html(content);
 	const player = new Plyr('#player');
